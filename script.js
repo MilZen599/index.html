8aaeeaa67d1bfcebb1e4a9c8c8f20f1d6802957d
MilZen599 <miloud-zenati@hotmail.fr>
2024-08-06 11:06:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
 // Vidéo se lance automatiquement
 document.addEventListener('DOMContentLoaded', function() {
-    const video = document.getElementById('myVideo');
+            const video = document.getElementById('myVideo');
 
-    if (video) { // Vérifie si l'élément vidéo existe
-        const observer = new IntersectionObserver((entries, observer) => {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    video.play();
-                } else {
-                    video.pause();
-                }
-            });
-        }, { threshold: 0.5 });
+            const observer = new IntersectionObserver((entries, observer) => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        video.play();
+                    } else {
+                        video.pause();
+                    }
+                });
+            }, { threshold: 0.5 });
 
-        observer.observe(video);
-    } else {
-        console.error("L'élément vidéo avec l'ID 'myVideo' n'a pas été trouvé.");
-    }
-});
+            observer.observe(video);
+        });
+
 // Fin Vidéo se lance automatiquement
