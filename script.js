ac1e7e8ef94ca46ad70c1447022b0a6291796bdd
MilZen599 <miloud-zenati@hotmail.fr>
2024-08-06 09:05:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 
+// Video se lance automqtiquement
+document.addEventListener('DOMContentLoaded', function() {
+            const video = document.getElementById('myVideo');
+
+            const observer = new IntersectionObserver((entries, observer) => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        video.play();
+                    } else {
+                        video.pause();
+                    }
+                });
+            }, { threshold: 0.5 });
+
+            observer.observe(video);
+        });
+
+// Fin Video se lance automqtiquement
