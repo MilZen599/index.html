9cb5013d2ac491c773a484152b7d4dd04a41805e
MilZen599 <miloud-zenati@hotmail.fr>
2024-08-07 14:57:44 -0400

    Create scriptslider.js

---
+++ scriptslider.js
@@ -0,0 +1,31 @@
+const slider = new Vue({
+    el: '#slider',
+    data: () => ({
+        val: 70
+    }),
+    mounted() {
+        this.val = Math.floor(Math.random() * 101);
+    },
+    computed: {
+        getPlacement() {
+            return ((this.val * 14.5)) + `%`;
+        },
+        greaterThanFifty() {
+            return this.val > 50 ? `var(--roundness)` : `0`;
+        },
+        getHappinessColor() {
+            return `rgba(255, ${106 + (103 / 100 * this.val)}, ${(Math.floor(this.val * -1 / 7.692)) + 13}`;
+        },
+        getSliderBackground() {
+            return `linear-gradient(to right, var(--orange), ${(this.val * -1) + 125}%, var(--yellow))`;
+        },
+        getHappiness() {
+            let moods = ["😡","😠","😦","☹️","🙁","😐","🙂","😊","😄","😃","😍"]
+			
+			if (this.val === 0) {
+				return "🤬";
+			}
+            return moods[(Math.floor(this.val / 10))];
+        }
+    }
+});
